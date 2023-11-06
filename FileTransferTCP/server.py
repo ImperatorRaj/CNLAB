8d49e31ff415416dce6060666a5dcebd19a1257d
ImperatorRaj <150010073+ImperatorRaj@users.noreply.github.com>
2023-11-06 21:33:13 +0530

    Update server.py

--- FileTransferTCP/server.py
+++ FileTransferTCP/server.py
@@ -1,47 +1,30 @@
-
 import socket
 
-IP = socket.gethostbyname(socket.gethostname())
-PORT = 3037
-ADDR = (IP, PORT)
-SIZE = 1024
-FORMAT = "utf-8"
+IP = "127.0.0.1"
+PORT = 5555
 
-def main():
-    print("[STARTING] Server is starting.")
-    """ Staring a TCP socket. """
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((IP, PORT))
+server.listen()
 
-    """ Bind the IP and PORT to the server. """
-    server.bind(ADDR)
+print("[LISTENING] Server is listening")
 
-    """ Server is listening, i.e., server is now waiting for the client to connected. """
-    server.listen()
-    print("[LISTENING] Server is listening.")
+while True:
+    conn, addr = server.accept()
+    print(f"[NEW CONNECTION] {addr} connected")
 
-    while True:
-        """ Server has accepted the connection from the client. """
-        conn, addr = server.accept()
-        print(f"[NEW CONNECTION] {addr} connected.")
+    filename = conn.recv(1024).decode()
+    print(f"[RECV] Receiving the filename")
 
-        """ Receiving the filename from the client. """
-        filename = conn.recv(SIZE).decode(FORMAT)
-        print(f"[RECV] Receiving the filename.")
-        file = open(filename, "w")
-        conn.send("Filename received.".encode(FORMAT))
+    file = open(f"received.txt", "w")
+    conn.send("Filename received.".encode())
 
-        """ Receiving the file data from the client. """
-        data = conn.recv(SIZE).decode(FORMAT)
-        print(f"[RECV] Receiving the file data.")
-        file.write(data)
-        conn.send("File data received".encode(FORMAT))
+    data = conn.recv(1024)
+    print("Content of data is ",data.decode())
+    print(f"[RECV] Receiving the file data")
+    file.write(data.decode())
+    conn.send("File data received".encode())
 
-        """ Closing the file. """
-        file.close()
-
-        """ Closing the connection from the client. """
-        conn.close()
-        print(f"[DISCONNECTED] {addr} disconnected.")
-
-if __name__ == "__main__":
-    main()
+    file.close()
+    conn.close()
+    print(f"[DISCONNECTED] {addr} disconnected")
